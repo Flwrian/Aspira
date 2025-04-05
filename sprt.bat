f58de78232b1ceb730e3101b487c66069c26365f
Flwrian <floflomontage@gmail.com>
2025-04-06 01:26:30 +0200

    3foldrep + qsearch

--- sprt.bat
+++ sprt.bat
@@ -3,7 +3,6 @@
 -engine cmd="Aspira_main.exe" name=main ^
 -each proto=uci tc=8+0.08 ^
 -games 50 ^
--log file="aaa" ^
 -recover ^
 -sprt elo0=0 elo1=5 alpha=0.05 beta=0.1 ^
 -openings file="C:\Users\flofl\Desktop\Code\chess-java\Nouveau dossier\fastchess-windows-latest\8moves_v3.epd" format=epd ^
