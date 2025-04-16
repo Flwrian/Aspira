3bdfaa30e0b2531eef0870837b11549d8035423f
Flwrian <floflomontage@gmail.com>
2025-04-16 21:17:08 +0200

    tt move ordering

--- sprt.bat
+++ sprt.bat
@@ -6,4 +6,4 @@
 -recover ^
 -sprt elo0=0 elo1=5 alpha=0.05 beta=0.1 ^
 -openings file="C:\Users\flofl\Desktop\Code\chess-java\Nouveau dossier\fastchess-windows-latest\8moves_v3.epd" format=epd ^
--concurrency 7
+-concurrency 5
