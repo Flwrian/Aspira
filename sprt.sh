3fa44bae230c1a46db1191abd79683f263c2954b
Flwrian <floflomontage@gmail.com>
2026-01-13 05:11:18 +0100

    delete old files

--- sprt.sh
+++
@@ -1,1 +0,0 @@
-./fastchess-ubuntu-22.04 -engine cmd="./Aspira_dev.exe" name=dev -engine cmd="./engines/Aspira_dev_14.exe" name=main -each proto=uci tc=4+0.04 -games 150 -recover -sprt elo0=0 elo1=5 alpha=0.05 beta=0.1 -openings file="./8moves_v3.epd" format=epd -concurrency 7
